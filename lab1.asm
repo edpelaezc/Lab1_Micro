9b79662516de25151f1e534088468e75d76e08ac
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2019-09-19 15:34:53 -0600

    Lab 1

---
+++ lab1.asm
@@ -0,0 +1,38 @@
+.MODEL small 
+.DATA
+; variable que contiene el texto que qeuremos mostrar
+nombre DB 'EDUARDO PELAEZ $'; $ significa el final de la cadena 
+carnet DB '1096917$'
+texto DB ' el valor a imprimir es:$'
+
+
+.STACK
+.CODE
+Programa: ;etiqueta de inicio de programa
+;inicializar programa
+
+MOV AX, @DATA ; guardando direcciòn de inicio segmento de datos
+MOV DS, AX
+
+; imprimir cadena
+
+MOV DX, OFFSET nombre ; asignando a DX la variable cadena
+MOV AH, 09h ; decimos que se imprimira una cadena
+INT 21h ; ejecuta la interrupciòn, imprimira
+
+MOV DX, OFFSET carnet ; asignando a DX la variable cadena
+MOV AH, 09h ; decimos que se imprimira una cadena
+INT 21h ; ejecuta la interrupciòn, imprimira
+
+MOV DX, OFFSET texto ; asignando a DX la variable cadena
+MOV AH, 09h ; decimos que se imprimira una cadena
+INT 21h ; ejecuta la interrupciòn, imprimira
+
+MOV AH, 02H
+MOV DX, 27
+INT 21h
+
+; finalizar el programa
+MOV AH, 4ch ; finalizar el proceso 
+INT 21h ; ejecuta la interrupciòn 
+END Programa
